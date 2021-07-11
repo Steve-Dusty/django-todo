e532c1ac9c48f0c9d6b159e9b7b3f8e81d4835e6
Steve-Dusty <kuantingk2@gmail.com>
2021-07-10 19:56:15 -0700

    finished main functionality of todo app

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views 
 urlpatterns = [
-    path('', views.index),
-    path('todo/', views.todoPanel)
+    path('', views.index, name='home'),
+    path('panel/', views.todoPanel, name='panel'),
+    path('addTask', views.addTask, name='addTask'),
+    path('toggleTask/<int:task_id>', views.toggleTask, name='toggleTask'),
+    path('deleteTask/<int:task_id>', views.deleteTask, name='deleteTask'),
+    path('clearAll', views.clearAll, name='clearAll')
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,22 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
 from .forms import TodoForm
+from django.views.decorators.http import require_POST
 
 def index(request):
     return render(request, 'home.html')
 
 
 def todoPanel(request):
+    form = TodoForm()
+    
+    todo_list = Todo.objects.order_by('-id')
+
+    context =  {'form': form, 'tasks': todo_list}
+
+    return render(request, 'todo_panel.html', context)
+
+@require_POST
+def addTask(request):
     if request.method == 'POST':
         form = TodoForm(request.POST)
+
         if form.is_valid():
             task = Todo(task=form.cleaned_data['task'])
             task.save()
-    else:
-        form = TodoForm()
-    
-    data = Todo.objects.all()
 
-    context =  {'form': form, 'tasks':data}
+        return redirect('panel')
 
-    return render(request, 'todo_panel.html', context)
+def toggleTask(request, task_id):
+    task = Todo.objects.get(pk=task_id)
+    task.is_completed = not task.is_completed
+    task.save()
+    return redirect('panel')
+
+def deleteTask(request, task_id):
+    task = Todo.objects.get(pk=task_id)
+    task.delete()
+    return redirect('panel')
+
+def clearAll(request):
+    Todo.objects.all().delete()
+
+    return redirect('panel')
