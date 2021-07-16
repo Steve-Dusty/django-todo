7d6fd6968906d338b407047eb6e3019d9c746744
Steve-Dusty <kuantingk2@gmail.com>
2021-07-16 10:30:55 -0700

    change env to Env

--- todolist/settings.py
+++ todolist/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': env.dj_db_url('DATABASE_URL')
+    'default': Env.dj_db_url('DATABASE_URL')
 }
 
 
