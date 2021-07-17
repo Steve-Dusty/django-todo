0abb02ae540d98cbe6d2e60d6c7541eab96a8032
Steve-Dusty <kuantingk2@gmail.com>
2021-07-17 12:43:38 -0700

    deleted staticfiles

--- staticfiles/js/script.js
+++
@@ -1,8 +0,0 @@
-const field = document.querySelector('[name="room_code"]');
-
-field.addEventListener('keypress', function (event) {  
-   let key = event.keyCode;
-    if (key === 32) {
-      event.preventDefault();
-    }
-});
